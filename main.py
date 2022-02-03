22912b666f8636251f1b9fd020493c711c56afcd
Vaghela divyesh <86907707+iamdivyeshh@users.noreply.github.com>
2022-02-03 12:30:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ async def start_bot():
     await idle()
     print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
-    await bot.join_chat("AlishaSupport")
+    await bot.join_chat("HINDICHATINGGROUP")
     await blaze.send_message(
                SUPPORT,
             "<b>Congrats!! Music Bot has started successfully!</b>",
